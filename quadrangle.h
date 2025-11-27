c85612030d958d9454b72cf2aad25c6f54e26b48
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-11-27 11:53:06 +0300

    add Quadrangle base interface

---
+++ quadrangle.h
@@ -0,0 +1,27 @@
+#ifndef QUADRANGLE_H
+#define QUADRANGLE_H
+#include "Planar.h"
+#include "point.h"
+
+
+struct Quadrangle: Planar {
+  Quadrangle(Point aa, Point bb, Point cc, Point dd);
+  ~Quadrangle();
+
+  virtual int x() const;
+  virtual int y() const;
+  virtual int abs_sqr() const;
+  virtual double get_area() const;
+  virtual frame_t get_frame() const;
+  Point first() const;
+  Point a() const;
+  Point b() const;
+  Point c() const;
+  Point d() const;
+
+  private:
+    Point data[4];
+};
+
+
+#endif
